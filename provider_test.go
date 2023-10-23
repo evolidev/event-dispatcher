00c444f82068e9a75cff5997c28724112938e382
Christian Marbach <cma@evoli.dev>
2023-10-23 10:04:56 +0200

    Fix test

--- provider_test.go
+++ provider_test.go
@@ -4,17 +4,17 @@ import "testing"
 
 func TestTreeProviderShouldBeAbleToCallListenerForSpecificEvent(t *testing.T) {
 	tlp := NewTreeProvider()
-	tlp.Add("test.event.one", ListenerFunc(func(event Event) (Event, error) {
+	tlp.Add("event_dispatcher.test.event.one", ListenerFunc(func(event Event) (Event, error) {
 		return event, nil
 	}))
-	tlp.Add("test.event.two", ListenerFunc(func(event Event) (Event, error) {
+	tlp.Add("event_dispatcher.test.event.two", ListenerFunc(func(event Event) (Event, error) {
 		return event, nil
 	}))
 
 	l := tlp.GetListenersForEvent(&testEventOne{})
 
 	if len(l) != 1 {
-		t.Errorf("Expected exatly one listener")
+		t.Errorf("Expected exatly one listener. Got %d", len(l))
 	}
 }
 
@@ -23,20 +23,20 @@ func TestTreeProviderShouldBeAbleToReturnAllListenerForSubType(t *testing.T) {
 	tlp.Add("", ListenerFunc(func(event Event) (Event, error) {
 		return event, nil
 	}))
-	tlp.Add("test", ListenerFunc(func(event Event) (Event, error) {
+	tlp.Add("event_dispatcher.test", ListenerFunc(func(event Event) (Event, error) {
 		return event, nil
 	}))
-	tlp.Add("test.event", ListenerFunc(func(event Event) (Event, error) {
+	tlp.Add("event_dispatcher.test.event", ListenerFunc(func(event Event) (Event, error) {
 		return event, nil
 	}))
-	tlp.Add("test.event.one", ListenerFunc(func(event Event) (Event, error) {
+	tlp.Add("event_dispatcher.test.event.one", ListenerFunc(func(event Event) (Event, error) {
 		return event, nil
 	}))
 
 	l := tlp.GetListenersForEvent(&testEventOne{})
 
 	if len(l) != 4 {
-		t.Errorf("Expected exatly %d listener", 4)
+		t.Errorf("Expected exatly %d listener. Got %d", 4, len(l))
 	}
 }
 
